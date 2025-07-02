f809bbaa139e154133dd01fad0fee5757dc0c45e
Lucas Mendes Leite <digitalartist2013@gmail.com>
2025-07-02 00:06:54 -0300

    Ajustes para evitar warnings

--- project-forest-hero/Scenes/Stage/moving_platform.gd
+++ project-forest-hero/Scenes/Stage/moving_platform.gd
@@ -7,7 +7,7 @@ func _ready():
 	start_tween()
 
 func start_tween():
-	var tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	var tween = create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
 	tween.tween_property($AnimatableBody2D, "position", offset, duration / 2)
 	tween.tween_property($AnimatableBody2D, "position", Vector2.ZERO, duration / 2)
